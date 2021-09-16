25baea0f06ac911cd84da3ac8d40777aa72d8f08
Mootassam <mootassame@gmail.com>
2021-09-16 09:51:26 +0100

    add bootstrap and jquery

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import "./App.css";
 import RoutesComponent from "./view/routes/RoutesComponent";
 import { BrowserRouter } from "react-router-dom";
+import jQuery from "jquery";
+import "bootstrap/dist/js/bootstrap";
+(window as any).$ = (window as any).jQuery = jQuery;
 function App() {
   return (
     <BrowserRouter>
