261e4eb64dc7d66b9dcf856ec1b07fa6d21ac07b
Mootassam <mootassame@gmail.com>
2021-09-21 22:25:27 +0100

    add ContactListSelectors and ContactLisActions

--- src/modules/contact/list/ContactListActions.tsx
+++ src/modules/contact/list/ContactListActions.tsx
@@ -1,27 +1,36 @@
-// import ContactService from "../contactService";
+import ContactService from "../contactService";
+import selectors from "src/modules/contact/list/ContactListSelectors";
+const prefix = "CONTACT_LIST";
+const ContactListActions = {
+  LIST_STARTED: `${prefix}_LIST_STARTED`,
+  LIST_SUCCESS: `${prefix}_LIST_SUCCESS`,
+  LIST_ERROR: `${prefix}_LIST_ERROR`,
 
-// const prefix = "CONTACT_LIST";
-// const ContactListActions = {
-//   LIST_STARTED: `${prefix}_LIST_STARTED`,
-//   LIST_SUCCESS: `${prefix}_LIST_SUCCESS`,
-//   LIST_ERROR: `${prefix}_LIST_ERROR`,
+  doFind:
+    (filter?, rawFilter?, keepPagination = false) =>
+    async (dispatch, getState) => {
+      try {
+        dispatch({
+          type: ContactListActions.LIST_STARTED,
+          payload: { filter, rawFilter, keepPagination },
+        });
+        const response = await ContactService.list(
+          filter,
+          selectors.selectOrderBy(getState()),
+          selectors.selectLimit(getState()),
+          selectors.selectOffset(getState())
+        );
 
-//   doFind: (filter,rawFilter,keepPagination= false) => async (dispatch) => {
-//     try {
-//       dispatch({ type: ContactListActions.LIST_STARTED });
-//       dispatch({ type: ContactListActions.LIST_SUCCESS });
-//       await ContactService.list(filter, rawFilter?, limit, offset);
-//     } catch (error) {
-//       dispatch({ type: ContactListActions.LIST_ERROR });
-//     }
-//   },
-// };
-// export default ContactListActions;
-
-import React from "react";
-
-function ContactListActions() {
-  return <div></div>;
-}
-
+        dispatch({
+          type: ContactListActions.LIST_SUCCESS,
+          payload: {
+            rows: response.rows,
+            count: response.count,
+          },
+        });
+      } catch (error) {
+        dispatch({ type: ContactListActions.LIST_ERROR });
+      }
+    },
+};
 export default ContactListActions;
--- src/modules/contact/list/ContactSelectors.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function ContactSelectors() {
-  return <div></div>;
-}
-
-export default ContactSelectors;
