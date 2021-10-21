0425c5a0dcf01e1ac568537b48e550ae118dfbd8
Mootassam <mootassame@gmail.com>
2021-10-21 13:59:07 +0100

    add ListLoading before showing list education

--- src/modules/education/list/EducationListSelectors.tsx
+++ src/modules/education/list/EducationListSelectors.tsx
@@ -1,7 +1,14 @@
 import { createSelector } from "reselect";
 const selectRaw = (state) => state.education.list;
 const selectRows = createSelector([selectRaw], (record) => record.rows);
+
+const listLoading = createSelector(
+  [selectRaw],
+  (loading) => loading.listLoading
+);
+
 const EducationListSelectors = {
   selectRows,
+  listLoading,
 };
 export default EducationListSelectors;
--- src/view/Education/list/EducationListTable.tsx
+++ src/view/Education/list/EducationListTable.tsx
@@ -2,38 +2,45 @@ import React, { useEffect } from "react";
 import actions from "src/modules/education/list/EducationListActions";
 import { useDispatch, useSelector } from "react-redux";
 import selectors from "src/modules/education/list/EducationListSelectors";
+import Spinner from "../../shared/Spinner/Spinner";
 function EducationListTable() {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(actions.doFetch());
   }, [dispatch]);
   const rows = useSelector(selectors.selectRows);
+  const listLoading = useSelector(selectors.listLoading);
   return (
     <>
-      {rows.map((rows) => (
-        <div className='row'>
-          <div className='col-lg-12'>
-            <div className='card card-large-icons'>
-              <div className='card-icon bg-primary text-white'>
-                <i className='fas fa-cog'></i>
-              </div>
-              <div className='card-body'>
-                <h4>General</h4>
-                <p>
-                  General settings such as, site title, site description,
-                  address and so on. General settings such as, site title, site
-                  description, address and so on. General settings such as, site
-                  title, site description, address and so on. General settings
-                  such as, site title, site description, address and so on.
-                </p>
-                <a href='features-setting-detail.html' className='card-cta'>
-                  Change Setting <i className='fas fa-chevron-right'></i>
-                </a>
+      {listLoading && <Spinner />}
+      {!listLoading &&
+        rows.map((rows) => (
+          <div className='row'>
+            <div className='col-lg-12'>
+              <div className='card card-large-icons'>
+                <div className='card-icon bg-primary text-white'>
+                  <i className='fas fa-cog'></i>
+                </div>
+                <div className='card-body'>
+                  <h4>
+                    {rows.degrree}at {rows.school}
+                  </h4>
+                  <p>
+                    General settings such as, site title, site description,
+                    address and so on. General settings such as, site title,
+                    site description, address and so on. General settings such
+                    as, site title, site description, address and so on. General
+                    settings such as, site title, site description, address and
+                    so on.
+                  </p>
+                  <a href='features-setting-detail.html' className='card-cta'>
+                    Change Setting <i className='fas fa-chevron-right'></i>
+                  </a>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
     </>
   );
 }
